daac1a61ce42eb56f9d94ec007e7affa12cdca03
Tierry Pierre <pierre.tierry40@gmail.com>
2022-11-22 12:12:12 -0500

    Pushed

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-
 //require("./db");
 const express = require("express");
 const hbs = require("hbs");
+const mongoose = require("mongoose");
+const path = require("path");
+
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 //require("./config")(app);
+
+// view engine setup
+app.set("view engine", "hbs");
+app.set('views', path.join(__dirname, 'views'));
+
 
 
 const countryRouter = require("./routes/country-route");
--- routes/country-route.js
+++ routes/country-route.js
@@ -2,24 +2,52 @@ const router = require("express").Router();
 const axios = require("axios");
 
 
-router.get("/", (req, res, next) => {
-    let countryName = req.query.countryName
-    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    axios.get(finalURL)
-    .then(responseFromAPI => {
-        console.log(responseFromAPI)
-        console.log(responseFromAPI.data)
-        res.send(responseFromAPI.data)
-    })
-    .catch((err)=>{
-console.log(err.response.data)
-res.send('error')
-    })
+// router.get("/", (req, res, next) => {
+//     let countryName = req.query.countryName
+//     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     
-}) 
+//     axios.get(finalURL)
+// .then(responseFromAPI => {
+//         console.log(responseFromAPI)
+//         console.log(responseFromAPI.data)
+//         res.send(responseFromAPI.data)
+//       res.render("layout.hbs");
+//     })
+//     .catch((err)=>{
+// console.log(err.response.data)
+// res.send('error')
+//     })
+    
+// }) 
 
-router.post("/searchCountry", (req, res, next) => {
+// router.get("/home")
 
+
+
+// router.get("/characters/:id", (req, res, next) => {
+//   axios
+//     .get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+//     .then((responseFromAPI) => {
+//       // console.log("details: ", responseFromAPI.data)
+//       res.render("characters/details-character", {
+//         character: responseFromAPI.data,
+//       });
+//     })
+//     .catch((err) => console.error(err));
+// });
+
+
+router.get("/", (req,res,next) => { 
+      res.render("home.hbs")
+    
+  })
+
+router.get("/searchCountry", (req, res, next) => {
+axios.get(
+       `https://restcountries.com/v3.1/name/${req.query.countryName}?fullText=true`
+     ) .then(responseFromAPI => {
+ res.send(responseFromAPI.data)
+     })
 
 })
 
